6a29c1137c44bf1c8be0a58599773784cf1347ef
kimspractice <tpghksdldia@naver.com>
2023-01-09 18:01:11 +0900

    how to find docs

--- houses/models.py
+++ houses/models.py
@@ -9,4 +9,9 @@ class House(models.Model):
     price_per_night = models.PositiveIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=140)
-    pets_allowed = models.BooleanField(default=True)
+    pets_allowed = models.BooleanField(
+        verbose_name="Pets Allowed ?",
+        default=True, help_text="Pets Allowed")
+
+    def __str__(self):
+        return self.name
