44f848f20ad000ee260b86aefce6bd640e9d1006
킴뽀삐 <Kimpoppy@MacBookPro.local>
2023-03-20 15:56:43 +0900

    Bookings

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,7 @@ CUSTOM_APPS = [
     "categories.apps.CategoriesConfig",
     "reviews.apps.ReviewsConfig",
     "wishlists.apps.WishlistsConfig",
+    "bookings.apps.BookingsConfig",
 ]
 # Application definition
 
